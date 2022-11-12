c0a335767eb07e6bb33aa4dc90514c97f48e3c34
Jorge Echeverria <coko.eche@gmail.com>
2022-11-12 23:47:07 +0100

    move html/css  from cards to card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,27 @@
 import './Card.css'
 
-const Card = ({children, type}) => {
+const Card = ({
+  pledge,
+  type}) => {
+  
+    const { title, subtitle, description, amount } = pledge
   return (
     <div className={`card-style ${type}`}>
-        {children}
+      <div className='card-title'>
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+      </div>
+
+      <p>{description}</p>
+
+      <div id="flex1">
+        <div id='flex2'>
+          <p className='num-left'>{amount}</p>
+          <p>left</p>
+        </div>
+                
+        <button>Select Reward</button>    
+      </div>
     </div>
   )
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,62 +1,37 @@
+import { useState } from 'react'
 import Card from './Card.jsx'
-import './Cards.css'
 
 const Cards = () => {
+
+    const [pledges, setPledges] = useState([
+        {
+         title:"Bamboo Stand",
+         subtitle:"Pledge $25 or more",
+         description:"You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list.",
+         amount:101  
+        },
+        {
+            title:"Black Edition Stand",
+            subtitle:"Pledge $75 or more",
+            description:"You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+            amount:64  
+        },
+        {
+            title:"Mahogany Special Edition",
+            subtitle:"Pledge $200 or more",
+            description:"You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+            amount:0  
+        }
+    ])
+
   return (
     <>
-        <Card>
-            <div className='card-title'>
-                <h1>Bamboo Stand</h1>
-                <h2>Pledge $25 or more</h2>
-            </div>
-            <p>You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and 
-                you’ll be added to a special Backer member list.
-            </p>
-            <div id="flex1">
-                <div id='flex2'>
-                    <p className='num-left'>101 </p>
-                    <p>left</p>
-                </div>
-                
-                <button>Select Reward</button>    
-            </div>
-        </Card>  
-
-        <Card>
-            <div className="card-title">
-                <h1>Black Edition Stand</h1>
-                <h2>Pledge $75 or more</h2>
-            </div>
-            <p>You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer 
-  member list. Shipping is included.
-            </p>
-            <div id="flex1">
-                <div id='flex2'>
-                    <p className='num-left'>64 </p>
-                    <p>left</p>
-                </div>            
-                <button>Select Reward</button>  
-            </div>  
-        </Card>
-        
-        <Card
-        type='no-stock'
-        >
-            <div className="card-title">
-                <h1>Mahogany Special Edition</h1>
-                <h2>Pledge $200 or more</h2>
-            </div>
-            <p>You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added 
-  to our Backer member list. Shipping is included.
-            </p>
-            <div id="flex1">
-                <div id='flex2'>
-                    <p className='num-left'>0 </p>
-                    <p>left</p>
-                </div>            
-                <button>Out of Stock</button>  
-            </div>  
-        </Card> 
+        {pledges.map(pledge => (
+            <Card
+            pledge={pledge}
+            type={pledge.amount === 0 ? 'no-stock' : ''}
+            />
+        ))}
     </>
   )
 }
